3d917eb8639d046c687b0aba7a7cc95f79471039
RandomQWE <31191176+RandomQWE@users.noreply.github.com>
2019-07-30 22:27:21 +0530

    adding layout

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { HeaderComponent } from './components/header/header.component';
+import { LayoutRoutingModule } from './layout-routing.module';
+import { LayoutComponent } from './layout.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,TranslateModule,
+    NgbDropdownModule,LayoutRoutingModule
+  ],
+  declarations: [LayoutComponent, SidebarComponent, HeaderComponent]
 })
 export class LayoutModule { }
