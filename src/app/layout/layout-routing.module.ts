353a3821266a0b26477df3a2af23c82ff409d120
RandomQWE <31191176+RandomQWE@users.noreply.github.com>
2019-07-30 22:37:28 +0530

    test

--- src/app/layout/layout-routing.module.ts
+++ src/app/layout/layout-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
         component: LayoutComponent,
         children: [
             { path: '', redirectTo: 'dashboard', pathMatch: 'prefix' },
-            { path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) },
+            { path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) },]
         //     { path: 'charts', loadChildren: () => import('./charts/charts.module').then(m => m.ChartsModule) },
         //     { path: 'tables', loadChildren: () => import('./tables/tables.module').then(m => m.TablesModule) },
         //     { path: 'forms', loadChildren: () => import('./form/form.module').then(m => m.FormModule) },
