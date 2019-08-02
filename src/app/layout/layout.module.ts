a6379e473ef20d0bc62c0d9c0b3c2379afbb5e39
RandomQWE <31191176+RandomQWE@users.noreply.github.com>
2019-08-03 04:41:36 +0530

    everything working in admin now making faq page

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -7,6 +7,7 @@ import { LayoutRoutingModule } from './layout-routing.module';
 import { LayoutComponent } from './layout.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { Global } from '../shared/global';
+import { FaqComponent } from './faq/faq.component';
 //import { ContactComponent } from './contact/contact.component';
 
 
@@ -16,6 +17,6 @@ import { Global } from '../shared/global';
     CommonModule,
     NgbDropdownModule,LayoutRoutingModule
   ],
-  declarations: [LayoutComponent, SidebarComponent, HeaderComponent],providers:[Global]
+  declarations: [LayoutComponent, SidebarComponent, HeaderComponent, FaqComponent],providers:[Global]
 })
 export class LayoutModule { }
