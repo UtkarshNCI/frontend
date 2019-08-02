08f05434fa1625b1a8fbbcea01dc586dc7c0daa0
RandomQWE <31191176+RandomQWE@users.noreply.github.com>
2019-08-03 02:38:29 +0530

    globals

--- src/app/layout/components/header/header.component.ts
+++ src/app/layout/components/header/header.component.ts
@@ -8,7 +8,7 @@ import {Global} from '../../../shared/global';
     providers:[Global]
 })
 
-//var globals.isLoggedin=false;
+//var 
 
 export class HeaderComponent implements OnInit {
     public pushRightClass: string;
@@ -25,7 +25,6 @@ export class HeaderComponent implements OnInit {
                 this.toggleSidebar();
             }
         });
-        
         
     }
 
