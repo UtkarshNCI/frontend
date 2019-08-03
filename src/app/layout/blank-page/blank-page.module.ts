9ea481b7adfe92111ca16d2718e1b08d8abe8a12
RandomQWE <31191176+RandomQWE@users.noreply.github.com>
2019-08-03 05:59:28 +0530

    added some modules

--- src/app/layout/blank-page/blank-page.module.ts
+++ src/app/layout/blank-page/blank-page.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { BlankPageRoutingModule } from './blank-page-routing.module';
 import { BlankPageComponent } from './blank-page.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
-    imports: [CommonModule, BlankPageRoutingModule],
+    imports: [CommonModule, BlankPageRoutingModule,FlexLayoutModule],
     declarations: [BlankPageComponent]
 })
 export class BlankPageModule {
