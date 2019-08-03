a679e1ef4569f1bbe907bdb90af197957d2fc749
RandomQWE <31191176+RandomQWE@users.noreply.github.com>
2019-08-03 06:16:28 +0530

    Managed to make flex layout

--- src/app/layout/blank-page/blank-page.component.ts
+++ src/app/layout/blank-page/blank-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ImgSrcDirective, ImgSrcStyleBuilder } from '@angular/flex-layout';
 
 @Component({
     selector: 'app-blank-page',
@@ -6,8 +7,10 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./blank-page.component.scss']
 })
 export class BlankPageComponent implements OnInit {
-    listProduct:any=[{name:"a",description:"abc"},{name:"a",description:"abc"},{name:"a",description:"abc"},{name:"a",description:"abc"},{name:"a",description:"abc"},{name:"a",description:"abc"},{name:"a",description:"abc"},{name:"a",description:"abc"},{name:"a",description:"abc"},{name:"a",description:"abc"},{name:"a",description:"abc"}]
-    
+    listProduct:any=[{},{},,{},,{},,{},{},{},{},{},{},{},{},{},{}
+        ,{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}
+        ,{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}
+    ]
     
     constructor() {}
 
